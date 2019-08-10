10184523988ece4fc810735b7bd562f52b4d3f15
ShashankSR <shashanksheshan@gmail.com>
2019-08-10 16:47:34 +0530

    Set layout to main activity

--- app/src/main/java/com/example/weather/view/MainActivity.kt
+++ app/src/main/java/com/example/weather/view/MainActivity.kt
@@ -1,7 +1,17 @@
 package com.example.weather.view
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.weather.R
+import com.example.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).apply {
+
+        }
+    }
 }
