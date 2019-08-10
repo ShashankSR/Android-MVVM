c11b6ba4a365ec8902346954d9e58f720c4ce284
ShashankSR <shashanksheshan@gmail.com>
2019-08-10 14:30:22 +0530

    Add skeletal view model

---
+++ app/src/main/java/com/example/weather/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.weather.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class WeatherViewModel : ViewModel() {
+
+    data class CurrentViewObject(
+        val currentTemp: String,
+        val currentLocation: String
+    )
+
+    data class ForecastViewObject(
+        val temp: String,
+        val day: String
+    )
+
+    data class NetworkViewObject(
+        val showLoader: Int,
+        val showError: Int,
+        val showSuccess: Int,
+        val errorMessage: Int
+    )
+
+    lateinit var networkVO: LiveData<NetworkViewObject>
+    lateinit var currentVO: LiveData<CurrentViewObject>
+    lateinit var forecastVO: LiveData<List<ForecastViewObject>>
+
+}
