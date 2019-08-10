636ebadc247cccc8a1b6e761136e4241dd4f6667
ShashankSR <shashanksheshan@gmail.com>
2019-08-10 20:23:18 +0530

    Add GET Forecast API call

--- app/src/main/java/com/example/weather/data/WeatherApi.kt
+++ app/src/main/java/com/example/weather/data/WeatherApi.kt
@@ -1,3 +1,15 @@
 package com.example.weather.data
 
-interface WeatherApi
+import com.example.weather.BuildConfig
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+    @GET("forecast.json")
+    fun getForecast(
+        @Query("key") string: String = BuildConfig.API_KEY,
+        @Query("q") location: String = "Banglore",
+        @Query("days") days: Int = 4
+    ): Single<WeatherData>
+}
