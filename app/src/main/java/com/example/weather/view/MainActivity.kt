1dd7363c59996f2762f3d59fee9b30b7e29c211b
ShashankSR <shashanksheshan@gmail.com>
2019-08-11 11:29:09 +0530

    Add API to main screen

--- app/src/main/java/com/example/weather/view/MainActivity.kt
+++ app/src/main/java/com/example/weather/view/MainActivity.kt
@@ -7,18 +7,24 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.example.weather.R
+import com.example.weather.data.WeatherData
 import com.example.weather.databinding.ActivityMainBinding
 import com.example.weather.utils.ViewModelFactory
 import com.example.weather.viewmodel.WeatherViewModel
 import dagger.android.AndroidInjection
+import io.reactivex.SingleObserver
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SingleObserver<WeatherData> {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
 
     lateinit var weatherViewModel: WeatherViewModel
+
+    lateinit var compositeDisposable: CompositeDisposable
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -35,13 +41,36 @@ class MainActivity : AppCompatActivity() {
                 adapter = forecastAdapter
                 addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
             }
-
+            layoutError.tvRetry.setOnClickListener {
+                this@MainActivity.weatherViewModel.getForecast("").subscribe(this@MainActivity)
+            }
             weatherViewModel = this@MainActivity.weatherViewModel.apply {
                 forecastVO.observe(this@MainActivity, Observer {
                     forecastAdapter.updateData(it)
                 })
             }
         }
+
+        weatherViewModel.getForecast("").subscribe(this)
     }
 
+    override fun onSuccess(data: WeatherData) {
+        weatherViewModel.onSuccess(data)
+    }
+
+    override fun onSubscribe(d: Disposable) {
+        if (!this@MainActivity::compositeDisposable.isInitialized) {
+            compositeDisposable = CompositeDisposable()
+        }
+        compositeDisposable.add(d)
+    }
+
+    override fun onError(e: Throwable) {
+        weatherViewModel.onError()
+    }
+
+    override fun onStop() {
+        compositeDisposable.dispose()
+        super.onStop()
+    }
 }
