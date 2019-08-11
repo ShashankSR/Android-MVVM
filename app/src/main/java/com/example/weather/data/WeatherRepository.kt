aff20df42c6cffafe9ca39a0e67302fa0ad84523
ShashankSR <shashanksheshan@gmail.com>
2019-08-11 09:36:52 +0530

    Add API to repository

--- app/src/main/java/com/example/weather/data/WeatherRepository.kt
+++ app/src/main/java/com/example/weather/data/WeatherRepository.kt
@@ -1,5 +1,13 @@
 package com.example.weather.data
 
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
-class WeatherRepository @Inject constructor(val weatherApi: WeatherApi)
+class WeatherRepository @Inject constructor(val weatherApi: WeatherApi) {
+
+    fun getForecast(location: String) = weatherApi.getForecast(location = location)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.io())
+
+}
