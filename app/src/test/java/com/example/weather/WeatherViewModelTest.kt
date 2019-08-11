553aac0c424d8dd602573971b85c0517d92d52b8
ShashankSR <shashanksheshan@gmail.com>
2019-08-11 15:00:52 +0530

    Add rules, mock objects and psudo functions to test

--- app/src/test/java/com/example/weather/WeatherViewModelTest.kt
+++ app/src/test/java/com/example/weather/WeatherViewModelTest.kt
@@ -1,10 +1,38 @@
 package com.example.weather
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.weather.data.WeatherRepository
+import com.example.weather.viewmodel.WeatherViewModel
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class WeatherViewModelTest {
 
+    @Mock
+    lateinit var weatherRepository: WeatherRepository
+    lateinit var objectUnderTest: WeatherViewModel
+
+    @Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun beforeEachTests() {
+        objectUnderTest = WeatherViewModel(weatherRepository)
+    }
+
+    @Test
+    fun testOnSuccess() {
+
+    }
+
+    @Test
+    fun testOnError() {
+
+    }
 
 }
