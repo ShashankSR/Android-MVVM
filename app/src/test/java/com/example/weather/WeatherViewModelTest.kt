3186c78c2e40d741ecf769c75cac02c1650c9901
ShashankSR <shashanksheshan@gmail.com>
2019-08-11 14:50:40 +0530

    Add skeletal unit test

---
+++ app/src/test/java/com/example/weather/WeatherViewModelTest.kt
@@ -0,0 +1,10 @@
+package com.example.weather
+
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class WeatherViewModelTest {
+
+
+}
