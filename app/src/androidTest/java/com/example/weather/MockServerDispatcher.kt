f0efb80f4e70bf2a2cdc71411578ab07a98f2870
ShashankSR <shashanksheshan@gmail.com>
2019-08-12 07:44:22 +0530

    Add response as string instead of reading from file

--- app/src/androidTest/java/com/example/weather/MockServerDispatcher.kt
+++ app/src/androidTest/java/com/example/weather/MockServerDispatcher.kt
@@ -1,15 +1,8 @@
 package com.example.weather
 
-import com.example.weather.data.WeatherData
-import com.google.gson.Gson
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
-import java.io.BufferedReader
-import java.io.FileInputStream
-import java.io.FileNotFoundException
-import java.io.InputStreamReader
-import java.nio.charset.Charset
 
 class MockServerDispatcher {
 
@@ -30,12 +23,80 @@ class MockServerDispatcher {
 
 
     private fun loadData(): String? {
-        return try {
-            val path = MockServerDispatcher::class.java.getResource("/api-response/forecast.json")!!.path
-            val br = BufferedReader(InputStreamReader(FileInputStream(path), Charset.defaultCharset()))
-            Gson().fromJson<WeatherData>(br, WeatherData::class.java).toString()
-        } catch (ignore: FileNotFoundException) {
-            null
-        }
+        return "{\n" +
+            "  \"location\": {\n" +
+            "    \"name\": \"Paris\",\n" +
+            "    \"region\": \"Ile-de-France\",\n" +
+            "    \"country\": \"France\",\n" +
+            "    \"lat\": 48.87,\n" +
+            "    \"lon\": 2.33,\n" +
+            "    \"tz_id\": \"Europe/Paris\",\n" +
+            "    \"localtime_epoch\": 1565513433,\n" +
+            "    \"localtime\": \"2019-08-11 10:50\"\n" +
+            "  },\n" +
+            "  \"current\": {\n" +
+            "    \"last_updated\": \"2019-08-11 10:40\",\n" +
+            "    \"temp_c\": 17.0,\n" +
+            "    \"condition\": {\n" +
+            "    },\n" +
+            "    \"uv\": 5.0\n" +
+            "  },\n" +
+            "  \"forecast\": {\n" +
+            "    \"forecastday\": [\n" +
+            "      {\n" +
+            "        \"date\": \"2019-08-11\",\n" +
+            "        \"day\": {\n" +
+            "          \"avgtemp_c\": 18.8,\n" +
+            "          \"condition\": {\n" +
+            "          }\n" +
+            "        },\n" +
+            "        \"astro\": {\n" +
+            "        }\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"date\": \"2019-08-12\",\n" +
+            "        \"day\": {\n" +
+            "          \"avgtemp_c\": 18.7,\n" +
+            "          \"condition\": {\n" +
+            "          }\n" +
+            "        },\n" +
+            "        \"astro\": {\n" +
+            "        }\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"date\": \"2019-08-13\",\n" +
+            "        \"day\": {\n" +
+            "          \"avgtemp_c\": 19.0,\n" +
+            "          \"condition\": {\n" +
+            "          }\n" +
+            "        },\n" +
+            "        \"astro\": {\n" +
+            "        }\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"date\": \"2019-08-14\",\n" +
+            "        \"day\": {\n" +
+            "          \"avgtemp_c\": 20.3,\n" +
+            "          \"condition\": {\n" +
+            "          }\n" +
+            "        },\n" +
+            "        \"astro\": {\n" +
+            "        }\n" +
+            "      },\n" +
+            "      {\n" +
+            "        \"date\": \"2019-08-15\",\n" +
+            "        \"day\": {\n" +
+            "          \"avgtemp_c\": 21.2,\n" +
+            "          \"condition\": {\n" +
+            "          }\n" +
+            "        },\n" +
+            "        \"astro\": {\n" +
+            "        }\n" +
+            "      }\n" +
+            "    ]\n" +
+            "  },\n" +
+            "  \"alert\": {\n" +
+            "  }\n" +
+            "}"
     }
 }
